b8c64c8c3bf71b2821d7ebd125604fc4e78b964c
Fermen1305 <fermen1305@gmail.com>
2023-11-01 13:16:53 -0500

    api url

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,2 +1,2 @@
-// export const basePath:string = 'https://full-music.fermenf.com/api';
+// export const basePath:string = 'https://full-music-api-rc.fermenf.com/api';
 export const basePath:string = 'https://cors-anywhere.herokuapp.com/api.deezer.com';
